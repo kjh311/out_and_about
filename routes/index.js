2fece1a410866924744c8685b0fc56f46a0c69d5
Kevin Huelsmann <kjh311@gmail.com>
2016-05-01 09:45:44 -0700

    adding static routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Out and About' });
+  // res.send('I am the gate keeper!');
 });
+
+// GET user's page
+router.get('/user', function(req, res, next){
+  res.send('User Personal Page/Favorites!');
+});
+
+// GET Login page
+router.get('/login', function(req, res, next){
+  res.send('This is the login Page!');
+});
+
+// GET register page
+router.get('/register', function(req, res, next){
+  res.send('This is the register Page!');
+});
+
+// multiple locations
+router.get('/search', function(req, res, next){
+  res.send('Search Multiple Locations page');
+});
+
+// Single Location Page
+router.get('/location', function(req, res, next){
+  res.send('This is the login Page!');
+});
+
+// add location
+router.get('/add', function(req, res, next){
+  res.send('Add Location!');
+});
+
+// add personal info
+router.get('/edit', function(req, res, next){
+  res.send('Edit personal info screen!');
+});
+
+
+// don't know how to access this
+app.use('/admin', router);
+
 
 module.exports = router;
